93082761c728599f45d3dd4f2674135b51e1beab
thatmossymoo <grace.lafountain3@gmail.com>
2025-10-21 14:19:13 -0400

    Initial commit

---
+++ src/FavNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        SafeInput.getInt(in, "What is your favorite int?");
+
+    }
+}
